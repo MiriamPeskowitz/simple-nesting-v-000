fb4722af201860b7dfbd04dc534e4706a3965cea
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-23 19:05:07 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -69,7 +69,7 @@ def dennis_ritchies_language
 		 			}
 		 	 }
 
-dennis_ritchies_language = programmer_hash[:dennis_ritchie]{:languages}
+dennis_ritchies_language = programmer_hash[:dennis_ritchie][:languages]
 
 
 end
