d9ea2d3bf9500734e797df248488dded6f812a05
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-23 19:06:26 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -65,7 +65,7 @@ def dennis_ritchies_language
   dennis_ritchies_language = programmer_hash[:dennis_ritchie][:languages]
 
 end
-=begin
+
 def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
@@ -134,4 +134,4 @@ def adding_to_dennis
      }
 
 end
-=end
+
