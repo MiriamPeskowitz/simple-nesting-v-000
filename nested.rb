093716338511b2adc41731a523f91152cf0065ec
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-23 18:55:33 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require = 'pry'
 
 def hopper
 	programmer_hash =
@@ -62,6 +62,8 @@ def dennis_ritchies_language
      }
 
 programmer_hash[:dennis_ritchie][:languages] =
+binding.pry
+
 end
 =begin
 def adding_matz
